89620af4cd0d6f0d3f30a635488221f8675262fa
Scott Powers <scott.powers@bloomreach.com>
2026-03-27 11:01:30 -0600

    Bump version to v1.4.1

--- web/js/version.js
+++ web/js/version.js
@@ -1,6 +1,6 @@
 // App version - update these when making a release (see CLAUDE.md for semver rules)
 const VERSION_MAJOR = 1;
 const VERSION_MINOR = 4;
-const VERSION_PATCH = 0;
+const VERSION_PATCH = 1;
 
 const VERSION = `v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}`;
